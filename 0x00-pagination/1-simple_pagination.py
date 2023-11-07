8dfb48b743bc7220114320120ff7dafb5b60d7a9
WassCodeur <wachioubouraima56@gmail.com>
2023-11-07 15:23:55 +0000

    documented

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,8 +6,14 @@ import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size) -> Tuple:
-    """index_range
+def index_range(page: int, page_size: int) -> Tuple:
+    """index range
+    Args
+    page: int
+    page_size: int
+
+    Return
+    Tuple
     """
     idx_end = page * page_size
     idx_strt = idx_end - page_size
@@ -35,6 +41,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page 
+        Args
+        page: int
+        page_size: int
+
+        Return
+        List
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         idxs_strt, idxs_end = index_range(page, page_size)
