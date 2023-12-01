46208921e952ff3a258d9fb4d292093f54b51c1b
WassCodeur <wachioubouraima56@gmail.com>
2023-12-01 10:14:56 +0000

    mru replacement policies

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""mru cache replacement policy"""
+from collections import OrderedDict
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU cache
+    inherit from BaseCaching class
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """put data in chache
+
+        Arguments
+        ---------
+        key: str
+        item: any
+
+        Return
+        ------
+        None
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                _key = self.cache_data.popitem()
+                print(f"DISCARD: {_key[0]}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get iten by key
+
+        Argument
+        --------
+        key: str
+
+        Return
+        ------
+
+        self.cache_data[key] or None
+        """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key, last=True)
+            return self.cache_data[key]
+
+        return None
